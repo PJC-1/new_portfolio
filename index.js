7717060cb25b5f21e882da175988b3308dc5942a
PJC-1 <jumpzine@gmail.com>
2018-01-04 20:32:25 -0800

    created public files and added heroku deploy configs

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
   res.sendFile('views/index.html', {root: __dirname});
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log('now listening to smooth sounds of port 3000...');
 });
