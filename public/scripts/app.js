a69413daa8a5875c37a49a1b093985872dbcae79
PJC-1 <jumpzine@gmail.com>
2018-07-02 23:47:31 -0700

    navbar and media query setup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,10 @@
 console.log('testing app.js');
+
+function hamburger() {
+    var x = document.getElementById('navbar-id');
+    if(x.className === 'navbar') {
+        x.className += ' responsive';
+    } else {
+        x.className = 'navbar';
+    }
+}
